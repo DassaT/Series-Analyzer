56cc7e7d21e270d01a5b092de524cf69fbf9f663
DassaT <134342040+DassaT@users.noreply.github.com>
2023-06-14 17:53:04 +0300

    main.sh

--- main.sh
+++ main.sh
@@ -1,14 +1,35 @@
-@@ -25,19 +25,19 @@ function input_series() {
+#! /bin/bash
+
+function less_than_3() {
+    local arr=("$@")
+    local arr_len=${#arr[@]} 
+    if [[ $arr_len -lt 3 ]];
+    then    
+        echo -e "The number of arguments is not enough,\nenter at least 3 arguments"    
+        echo "0"
+    else
+        echo "1"
+    fi
+}
+
+function input_series() {
+    echo "Please a sequence of number seperated by spcae"
+    read sqnc
+    if [ $sqnc =~ '^[0-9 ]+$' ];
+    then
+        local input_array=(${sqnc[@]}) #($seqc)
+        echo "${input_array[@]}"
+    else
+        echo "0"
+    fi
 }
 
 function display_og_series() {
-   local arr="$@"
    local arr=("$@")
    echo ${arr[@]} 
 }
 
 function display_sorted_series() {
-    local arr="$@"
     local arr=("$@")
     local sorted=$(echo "${arr[@]}" | tr ' ' '\n' | sort -n)  
     # echo "Sorted numbers smallest to largest: "
@@ -16,16 +37,15 @@ function display_sorted_series() {
 }
 
 function print_max() {
-    local arr="$@"
     local arr=("$@")
     arr=$(display_soreted_series ${arr[@]})
     local len=${#arr[@]} # $(series_length ${arr[@]})
     local max_location=$(( $len-1 ))
-@@ -46,22 +46,22 @@ function print_max() {
+    local max=${arr[$max_location]}
+    echo $max
 }
 
 function print_min() {
-    local arr="$@"
     local arr=("$@")
     arr=$(display_soreted_series ${arr[@]})
     local min=${arr[0]}
@@ -33,10 +53,6 @@ function print_min() {
 }
 
 function print_avg() { 
-    local arr="$@"
-    local sum= $(sum_series ${arr[@]})
-    local len= $(series_length ${arr[@]})
-    avg=$(echo “scale=3;$sum/$len” | bc)
     local arr=("$@")
     local sum=$(sum_series ${arr[@]})
     local len=$(series_length ${arr[@]})
@@ -45,33 +61,66 @@ function print_avg() {
 }
 
 function series_length() {
-    local arr="$@"
     local arr=("$@")
     local len=${#arr[@]}
     echo  "$len"
 }
-@@ -76,7 +76,7 @@ function sum_series() {
+function sum_series() {
+    local arr=("$@")
+    local sum=0
+    for i in ${arr[@]};
+      do
+          sum=$(( $sum + $i ))
+      done
+    echo "$sum"
 }
 
 series="$@"
-is_valid=$(less_then_3 ${series[@]})
 is_valid=$(less_than_3 ${series[@]})
 if [ $is_valid -eq 0 ]
 then    
     series=$(input_series)
-@@ -109,12 +109,12 @@ do
+    series=($series)
+fi
+
+select operation in Input_Series OG_Series Sorted_Series Series_Max Series_Min Series_AVG Series_Length Series_Sum Exit ;
+do 
+    case $operation in 
+        Input_Series ) 
+            series=$(input_series)
+            series=($series)
+            ;;               
+        OG_Series )
+            display_og_series ${series[@]}
+            ;;
+        Sorted_Series ) 
+            display_sorted_series ${series[@]}
+            ;;
+        Series_Max ) 
+            max=$(print_max ${series[@]})
+            echo "The maximum value in the series is: $max"
+             ;;
+        Series_Min ) 
+            min=$(print_min ${series[@]})
+            echo "The maximum value in the series is: $min"
+             ;;
+         Series_AVG ) 
+            avg=$(print_avg ${series[@]})
             echo "The average of the series is: $avg"
              ;;
         Series_Length )
-            len=$(print_avg ${series[@]})
             len=$(series_length ${series[@]})
             echo "The length of the series is: $len"
             ;;
         Series_Sum )
-            sum=$(print_avg ${series[@]})
-            echo "The length of the series is: $sum"
             sum=$(sum_series ${series[@]})
             echo "The sum of the series is: $sum"
             ;;
         Exit )
             exit 
+            ;;
+        * )
+            echo "Invalid choice, try again!" 
+            ;;
+    esac    
+done
